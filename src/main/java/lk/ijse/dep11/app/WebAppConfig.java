e6f3bce1204fff09a6042d1bc199dbb1608d1fc5
Isuru Ashinsana <isuruashinsana@gmail.com>
2023-11-25 09:06:50 +0530

    Setup Hikari Connection Pool

--- src/main/java/lk/ijse/dep11/app/WebAppConfig.java
+++ src/main/java/lk/ijse/dep11/app/WebAppConfig.java
@@ -2,12 +2,13 @@ package lk.ijse.dep11.app;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
-@CrossOrigin
+@PropertySource("classpath:/application.properties")
 public class WebAppConfig {
 }
--- src/main/java/lk/ijse/dep11/app/api/CustomerHttpController.java
+++ src/main/java/lk/ijse/dep11/app/api/CustomerHttpController.java
@@ -1,14 +1,54 @@
 package lk.ijse.dep11.app.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PreDestroy;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 
 @RestController
 @RequestMapping("/customers")
+@CrossOrigin
+
 public class CustomerHttpController {
+    HikariDataSource pool;
+    public CustomerHttpController(Environment env) {
+
+        HikariConfig config=new HikariConfig();
+        config.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
+        config.setUsername(env.getRequiredProperty("spring.datasource.user"));
+        config.setPassword(env.getRequiredProperty("spring.datasource.password"));
+        config.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class-name"));
+        config.setMaximumPoolSize(env.getRequiredProperty("spring.datasource.hikari-maximum-size",Integer.class));
+
+        pool = new HikariDataSource(config);
+    }
+    @PreDestroy
+    private void destroy(){
+        pool.close();
+    }
+
     @GetMapping
-    public String getRespond(){
-        return "Hello";
+    public void getAllCustomers(String q){
+      
+
+    }
+    @GetMapping
+    public void getAllSortedCustomers(){
+
+    }
+    @GetMapping
+    public void getAllPaginatedCustomers(){
+
+    }
+    @GetMapping
+    public void getAllPaginatedAndSortedCustomers(){
+
     }
 }
